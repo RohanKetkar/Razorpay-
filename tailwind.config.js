8f8049ecbb6833838f4640f41810f1c65e8af258
Rohan Ketkar <yourgitemail@example.com>
2023-06-08 12:32:18 +0530

    maintain

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,8 @@
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {
-      fontFamily:{ mullish : ["Mullish","sans-serif"]},
-      backgroundImage: {
-        'hero-pattern': "url('/img/hero-pattern.svg')",
-        'footer-texture': "url('/img/footer-texture.png')",
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
